4ba7f9c940e5bf1a69d09f13b689d4c07aa95520
Mikhail Ianenko <mikhail.ianenko@t-systems.com>
2022-03-03 15:29:22 +0300

    first commit

--- first-pet/src/app/app.component.ts
+++ first-pet/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'first-pet project';
+  title: string = 'first pet project';
 }
